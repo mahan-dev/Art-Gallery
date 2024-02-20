9b0a49784e7c9341a26e2a4c96abfde0bcb2da2a
mahan.dev <mahan.dev.front@gmail.com>
2024-02-20 22:01:12 +0330

    fixing router issue hashrouter part-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { HashRouter as Router , Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Main from './components/Main';
 import Slider_image_Back from "./components/Slider_image_Back";
 import Slider_navigation from "./components/slider_navigation";
@@ -25,15 +25,17 @@ const App = () => {
   return (
     <>
       <Router>
-        <Route path="/Art-Gallery/Web3" component={Redirect_Web3} />
-        <Route path="/Store" component={Redirect_Store} />
-        <Route path="/Exhibitions" component={Redirect_Exhibitions} />
-        <Route path="/Voices" component={Redirect_Voices} />
-        <Route path="/News" component={Redirect_News} />
-        <Route path="/About" component={Redirect_About} />
-        <Route path="/contact" component={Redirect_Contact} />
-        <Route path="/artists" component={Redirect_Artists} />
-        <Route path="/" component ={wrapper_nav_imageBack} />
+        <Switch>
+          <Route path="/Art-Gallery/Web3" component={Redirect_Web3} />
+          <Route path="/Store" component={Redirect_Store} />
+          <Route path="/Exhibitions" component={Redirect_Exhibitions} />
+          <Route path="/Voices" component={Redirect_Voices} />
+          <Route path="/News" component={Redirect_News} />
+          <Route path="/About" component={Redirect_About} />
+          <Route path="/contact" component={Redirect_Contact} />
+          <Route path="/artists" component={Redirect_Artists} />
+          <Route path="/" component={wrapper_nav_imageBack} />
+        </Switch>
       </Router>
     </>
   );
