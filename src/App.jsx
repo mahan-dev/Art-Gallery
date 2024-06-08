2c7e837b3d15efbe44c9179c73754994205710e4
mahan.dev <mahan.dev.front@gmail.com>
2024-06-08 18:29:57 +0330

    adding Updates-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Redirect_Web3 from './components/Redirects/Redirect_Web3';
 import WrapperNavImageBack from './components/WrapperNavImageBack';
 import RedirectArtists from './components/Redirects/Redirect_Artists';
