e6fa14da9205bedd630ede8c3bfa3ffefa68551c
mahan.dev <mahan.dev.front@gmail.com>
2024-06-19 11:16:56 +0330

    Your commit message

--- src/components/ComponentNotFound.jsx
+++ src/components/ComponentNotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ComponentNotFound = () => {
     return (
         <div>
-            {console.log("owjgowjgopg")}
+     
             <section>we didn't found what you've searching for ......</section>
         </div>
     );
