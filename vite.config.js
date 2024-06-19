9e67666e532f3a52702d6f53eafede1acb53f924
mahan.dev <mahan.dev.front@gmail.com>
2024-06-19 20:29:34 +0330

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Art-Gallery",
-  // base: "https://mahan-dev.github.io/Art-Gallery",
+  // base: "/Art-Gallery",
+  base: "https://mahan-dev.github.io/Art-Gallery",
   // server: {
   //   fs: {
   //     strict: false,
