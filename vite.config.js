9fb7d297ad25e5c6ec56355a69e53e10e1bd0846
mahan.dev <mahan.dev.front@gmail.com>
2024-07-18 11:18:59 +0330

    redirect_contact_ responsive-2

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react()],
   // base: "/Art-Gallery",
   base: "https://mahan-dev.github.io/Art-Gallery",
+  git add .
   // server: {
   //   fs: {
   //     strict: false,
