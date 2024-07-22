25be030864158916430bad03eb89b10dab9e4967
mahan.dev <mahan.dev.front@gmail.com>
2024-07-22 22:01:01 +0330

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Redirect_Web3 from './components/Redirects/Redirect_Web3';
 import WrapperNavImageBack from './components/WrapperNavImageBack';
